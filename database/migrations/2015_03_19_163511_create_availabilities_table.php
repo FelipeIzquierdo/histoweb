8a06ef36219e2f7a1d25a45f5ff194174895ca34
Luis Felipe Izquierdo <felipeizquierdoo@gmail.com>
2015-03-23 12:00:42 -0500

    nuevos campos tabla availabilities

--- database/migrations/2015_03_19_163511_create_availabilities_table.php
+++ database/migrations/2015_03_19_163511_create_availabilities_table.php
@@ -11,12 +11,15 @@ class CreateAvailabilitiesTable extends Migration {
 		Schema::create('availabilities', function(Blueprint $table)
 		{
 			$table->increments('id');
+            $table->integer('id_availability');
 
             $table->timestamp('start');
             $table->timestamp('end');
 
+
             $table->integer('doctor_id')->unsigned();
             $table->foreign('doctor_id')->references('id')->on('doctors');
+            $table->enum('state',['available','used', 'discarded']);
 
 
 			
